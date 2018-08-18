bc35dd3b8c8205a54f2ea879b07584e9d21a6054
lesha K <alexei.jobs@gmail.com>
2018-08-18 13:30:07 +0300

    Recycler View basics. It is works!

--- app/src/main/java/com/example/android/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/android/shoppinglist/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.android.shoppinglist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUM_LIST_ITEMS = 100;
+    private GreenAdapter mAdapter;
+    private RecyclerView mItemsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mItemsList = (RecyclerView) findViewById(R.id.rv_items);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mItemsList.setLayoutManager(layoutManager);
+        mItemsList.setHasFixedSize(true);
+        mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+        mItemsList.setAdapter(mAdapter);
     }
 }
